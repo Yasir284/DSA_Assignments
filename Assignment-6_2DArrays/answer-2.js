ca344fd6ce9b02378d18e023d675ad3f2352d80f
Yasir284 <lambawalay@gmail.com>
2023-06-03 19:02:06 +0530

    answer 2 completed

--- Assignment-6_2DArrays/answer-2.js
+++ Assignment-6_2DArrays/answer-2.js
@@ -21,3 +21,31 @@ You must write a solution in O(log(m * n)) time complexity.
 
 </aside> */
 }
+let matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60],
+  ],
+  target = 3;
+
+function binarySearch(matrix, target) {
+  for (let i = 0; i < matrix.length; i++) {
+    let left = 0;
+    let right = matrix[i].length - 1;
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (matrix[i][mid] === target) return true;
+
+      if (matrix[i][mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return false;
+}
+console.log(binarySearch(matrix, target));
